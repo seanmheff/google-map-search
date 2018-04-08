f49555b060902174ad50d0adfb6df63ac94da0d3
Sean Heffernan <seanmheff@gmail.com>
2018-04-08 23:38:54 +0000

    Ship it

--- src/actions/index.js
+++ src/actions/index.js
@@ -23,27 +23,25 @@ export const loadMap = (div, input) => dispatch => {
       center: { lat: 23, lng: -18 }
     });
 
-
     // Create the search-box
     const searchBox = new google.maps.places.SearchBox(input);
 
-    // Update store with state
+    // Tell the store we have loaded the map ok
     dispatch(mapInitialized());
 
     // Placeholder for a marker
     let marker = null;
 
-    // Add relivant listeners to the search-box
+    // Add a listener for a user selecting a place
     searchBox.addListener('places_changed', () => {
       const places = searchBox.getPlaces();
 
-      // Guard against bugs
+      // Guard against no matches
       if (places && places.length === 0) return;
 
       // Just focus on the first place
       const place = places[0];
 
-      // Check. TODO
       if (!place.geometry) return;
 
       // Set the map bounds - focus in on result
@@ -55,26 +53,22 @@ export const loadMap = (div, input) => dispatch => {
       }
       map.fitBounds(bounds);
 
-      // Get the icon, name and location.
-      const icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
       // Get rid of old marker
-      if (marker) {
-        marker.setMap(null);
-      }
+      if (marker) { marker.setMap(null); }
 
       // Create a marker.
       marker = new google.maps.Marker({
         map: map,
-        icon: icon,
         title: place.name,
         position: place.geometry.location
+      });
+
+      // Show a message when the user clicks on the marker
+      const infoWindow = new google.maps.InfoWindow({
+        content: place.name
+      });
+      marker.addListener('click', () => {
+        infoWindow.open(map, marker);
       });
     });
   });
